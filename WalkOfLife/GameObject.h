b618a3ac60f0a09937ab97dc43db420038aaaa34
narkos <narkos.newb@gmail.com>
2015-05-05 14:33:01 +0200

    Snopp

--- WalkOfLife/GameObject.h
+++ WalkOfLife/GameObject.h
@@ -2,6 +2,31 @@
 
 #include "Entity.h"
 
+struct MatInfo
+{
+	MatInfo()
+		: Emissive(0.0f, 0.0f, 0.0f, 1.0f),
+		Ambient(0.1f, 0.1f, 0.1f, 1.0f),
+		Diffuse(1.0f, 1.0f, 1.0f, 1.0f),
+		Specular(1.0f, 1.0f, 1.0f, 1.0f),
+		SpecPow(128.0f),
+		UseTexture(false){}
+
+	XMFLOAT4 Emissive;
+	XMFLOAT4 Ambient;
+	XMFLOAT4 Diffuse;
+	XMFLOAT4 Specular;
+	float SpecPow;
+	bool UseTexture;
+	float Padding[2];
+
+
+};
+
+struct MaterialProperties
+{
+	MatInfo Material;
+};
 
 class GameObject : public Entity{
 public:
